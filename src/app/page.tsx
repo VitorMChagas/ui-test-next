dc5d326d6bfd36821ec84b7c71a14475153e8314
Vitor Chagas <vitormchagas@gmail.com>
2024-02-29 19:45:42 -0300

    feat: add created header components in main page and Head setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
-import NavBar from '@/components/navbar'
+import Head from 'next/head'
+
+import Header from '@/components/header'
+import SpeakOutButton from '@components/speak-out-button'
 
 export default function Page() {
-  return <NavBar></NavBar>
+  return (
+    <>
+      <Head>
+        <title>Rule of Thumb</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta content="Rule of Thumb™ tracks the sentiment of their users on trending and controversial people from different fields, including politics, business, media and entertainment, etc. Here users have an easy way to share their opinion on each presented celebrity using vote" />
+      </Head>
+      <Header />
+      <SpeakOutButton />
+    </>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,20 @@ module.exports = {
       },
       fontFamily: {
         lato: ['"Lato"', 'regular'],
-        'lato-light': ['"Lato"', 'light'],
+      },
+      letterSpacing: {
+        tightest: '-.075em',
+      },
+      colors: {
+        'thumbsbg-green': 'rgb(60 187 180 / 0.8)',
+        'thumbsbg-green-hover': 'rgb(60 187 180 / 1)',
+        'thumbsbg-yellow': 'rgb(249 173 29 / 0.8)',
+        'thumbsbg-yellow-hover': 'rgb(249 173 29 / 1)',
+        'light-bg': 'rgb(255 255 255 / 0.4)',
+        'dark-bg': 'rgb(0 0 0 / 0.4)',
+        'dark-nav': 'rgb(0 0 0 / 0.8)',
+        'dark-gray': 'rgb(70 70 70 / 1)',
+        'light-gray': 'rgb(235 235 235 / 1)',
       },
     },
   },
