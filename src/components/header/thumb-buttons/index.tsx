79147ba01b1b6d88bf95797c6710752b368451f0
Vitor Chagas <vitormchagas@gmail.com>
2024-02-29 19:42:31 -0300

    feat: added thumbs buttons in header component

---
+++ src/components/header/thumb-buttons/index.tsx
@@ -0,0 +1,26 @@
+export default function ThumbButtons() {
+  return (
+    <div className="flex justify-between h-[85px] w-full z-10">
+      <button
+        className="w-1/2 h-auto bg-thumbsbg-green hover:bg-thumbsbg-green-hover"
+        aria-label="thumbs up"
+      >
+        <img
+          className="h-8 m-auto max-w-8"
+          src="/img/thumbs-up.svg"
+          alt="thumbs up"
+        />
+      </button>
+      <button
+        className="w-1/2 h-auto bg-thumbsbg-yellow hover:bg-thumbsbg-yellow-hover"
+        aria-label="thumbs down"
+      >
+        <img
+          className="h-8 m-auto max-w-8"
+          src="/img/thumbs-down.svg"
+          alt="thumbs down"
+        />
+      </button>
+    </div>
+  )
+}
