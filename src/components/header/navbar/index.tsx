58e0c4e0091dd115b8ded7173059f0b28062bde0
Vitor Chagas <vitormchagas@gmail.com>
2024-02-29 19:43:03 -0300

    chore: change navbar folder location to header tree

--- src/components/header/navbar/index.tsx
+++ src/components/header/navbar/index.tsx
@@ -1,7 +1,7 @@
 export default function NavBar() {
   return (
-    <div className="fixed z-[2] flex w-[100%] min-h-[10rem] justify-between px-4 py-0 top-0 bg-gradient-to-b from-zinc-800">
-      <div className="flex text-white justify-between w-[100%] items-center px-44">
+    <div className="fixed z-[2] flex w-full min-h-[10rem] justify-between px-28 py-0 top-0 bg-gradient-to-b from-dark-nav">
+      <div className="flex items-center justify-between w-full text-white px-44">
         <p className="text-[2rem] font-normal">Rule of Thumb.</p>
         <ul className="w-[410px] text-[18px] font-light justify-between flex items-center translate-x-0 bg-transparent top-12">
           <button>Past Trials</button>
