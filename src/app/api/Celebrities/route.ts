27a70c3e7f42f48db9c30fbb28397cdda7c22fa8
Vitor Chagas <vitormchagas@gmail.com>
2024-03-01 20:56:56 -0300

    fix: add db connection to celebrities route

--- src/app/api/Celebrities/route.ts
+++ src/app/api/Celebrities/route.ts
@@ -1,7 +1,10 @@
 import Celebrity from '@service/models/Celebrity'
+import connectToDatabase from '@service/utils/dbConnect'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
+  await connectToDatabase()
+
   try {
     const celebrities = await Celebrity.find()
     return NextResponse.json({ celebrities }, { status: 200 })
