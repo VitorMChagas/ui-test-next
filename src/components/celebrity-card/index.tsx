35eeac11a72854b81683d0d4f6625057eceae51a
Vitor Chagas <vitormchagas@gmail.com>
2024-03-02 18:05:03 -0300

    feat: finalize style for CelebrityCard component

--- src/components/celebrity-card/index.tsx
+++ src/components/celebrity-card/index.tsx
@@ -1,43 +1,93 @@
 'use client'
-import React, { useState, useEffect, Fragment } from 'react'
+import { Fragment } from 'react'
+import { formatDistanceStrict } from 'date-fns/formatDistanceStrict'
 import Image from 'next/image'
-import { Celebrity } from './types'
+import ThumbsUp from '@components/thumbs-up'
+import ThumbsDown from '@components/thumbs-down'
+import VoteGauge from '@components/vote-gauge'
+import { useCelebrities } from '@hooks/useCelebrities'
+
+interface HandleVoteType {
+  id: number
+  type: string
+}
 
 export default function CelebrityCard() {
-  const [celebrities, setCelebrities] = useState<Celebrity[]>([])
+  const { celebrities } = useCelebrities()
 
-  useEffect(() => {
-    fetch('http://localhost:3000/api/Celebrities')
-      .then((res) => res.json())
-      .then((data) => {
-        console.log('API Response:', data.celebrities[0].data) // Log the API response
-        setCelebrities(data.celebrities[0].data)
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error)
-      })
-  }, [])
+  const handleVote = async (_id, type) => {
+    try {
+      // Convert the _id to string
+      const celebrityId = String(_id)
 
-  console.log('Celebrities State:', celebrities) // Log the celebrities state
+      const response = await fetch('/api/votes/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ celebrityId, type }), // Use celebrityId instead of _id
+      })
+
+      if (response.ok) {
+        const data = await response.json()
+        console.log('Vote registration success:', data)
+      } else {
+        console.error('Vote registration error:', response.statusText)
+      }
+    } catch (error) {
+      console.error('Vote registration error:', error)
+    }
+  }
 
   return (
-    <div>
-      {celebrities.map((celebrity) => (
-        <div className="w-[348px] h-[348px] text-white grid grid-cols-1">
+    <div className="grid grid-cols-3 justify-items-center w-[1100px]">
+      {celebrities?.map((celebrity) => (
+        <div className="w-[348px] h-[348px] text-white mb-10 flex flex-col justify-end">
           <Fragment key={celebrity._id}>
             <Image
               src={`/img/${celebrity.picture}`}
               alt={celebrity.name}
               width={348}
               height={348}
+              objectFit="cover"
               className="absolute -z-10"
             />
-            <p className="text-4xl">{celebrity.name}</p>
-            <p className="text-base font-normal">{celebrity.description}</p>
-            <p className="text-xs font-bold">{celebrity.category}</p>
-            <p>{celebrity.lastUpdated}</p>
-            <p>{celebrity.votes.positive}</p>
-            <p>{celebrity.votes.negative}</p>
+            <div className="flex max-h-[100px] flex-wrap items-center">
+              {celebrity.votes.positive > celebrity.votes.negative ? (
+                <ThumbsUp style="bg-thumbsbg-green" />
+              ) : (
+                <ThumbsDown style="bg-thumbsbg-yellow" />
+              )}
+              <p className="pl-2 overflow-hidden text-3xl font-normal whitespace-nowrap overflow-ellipsis max-w-[350px]">
+                {celebrity.name}
+              </p>
+            </div>
+
+            <p className="text-[15px] line-clamp-2 font-normal w-[300px] self-center">
+              {celebrity.description}
+            </p>
+            <p className="self-end pb-4 pr-5 text-xs font-bold">
+              {formatDistanceStrict(
+                new Date(celebrity.lastUpdated),
+                new Date(),
+              )}
+              &nbsp;ago in {celebrity.category}
+            </p>
+            <div className="flex justify-between w-[190px] self-end mr-8 mb-3">
+              <button onClick={() => handleVote(celebrity._id, 'positive')}>
+                <ThumbsUp style="bg-thumbsbg-green-hover" />
+              </button>
+              <button onClick={() => handleVote(celebrity._id, 'negative')}>
+                <ThumbsDown style="bg-thumbsbg-yellow-hover" />
+              </button>
+              <button className="w-[107px] h-[38px] border border-s-white bg-vote-now-bg text-[15px]">
+                Vote Now
+              </button>
+            </div>
+            <VoteGauge
+              positiveVotes={celebrity?.votes?.positive}
+              negativeVotes={celebrity?.votes?.negative}
+            />
           </Fragment>
         </div>
       ))}
