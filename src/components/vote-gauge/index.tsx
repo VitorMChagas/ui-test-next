2df2b269d5b3feba804ae896f573dc2c55cbdb3c
Vitor Chagas <vitormchagas@gmail.com>
2024-03-02 18:03:24 -0300

    feat: add listStyle for VoteGauge component

--- src/components/vote-gauge/index.tsx
+++ src/components/vote-gauge/index.tsx
@@ -4,37 +4,51 @@ import ThumbsUp from '@components/thumbs-up'
 interface VoteTypes {
   positiveVotes: number
   negativeVotes: number
+  isListView?: boolean
+  thumbStyle?: string
 }
-const VoteGauge = ({ positiveVotes, negativeVotes }: VoteTypes) => {
+
+export default function VoteGauge({
+  positiveVotes,
+  negativeVotes,
+  isListView,
+  thumbStyle,
+}: VoteTypes) {
   const totalVotes = positiveVotes + negativeVotes
   const positivePercentage =
     totalVotes > 0 ? (positiveVotes / totalVotes) * 100 : 0
   const negativePercentage = 100 - positivePercentage
 
-  const barStyle = {
+  const defaultStyle = {
     'min-width': '100%',
     width: `${positivePercentage}%`,
     background: `linear-gradient(to right, rgb(60 187 180 / 0.6) ${positivePercentage}%, rgb(249 173 29 / 0.6) ${positivePercentage}%)`,
-    height: '35px',
   }
 
   return (
     <>
-      <div style={barStyle}></div>
+      <div
+        className={isListView ? 'absolute bottom-0 h-[54px]' : 'h-[35px]'}
+        style={defaultStyle}
+      ></div>
       <div className="relative flex items-end">
-        <div className="w-[350px] justify-between flex absolute  px-1">
-          <div className="flex items-baseline">
-            <ThumbsUp />
+        <div
+          className={`justify-between flex absolute px-1 ${isListView ? 'w-[1100px] text-[27px] -bottom-[55px]' : 'w-[350px]'}`}
+        >
+          <div
+            className={`flex items-baseline justify-between ${isListView ? 'w-[110px] ml-[10px]' : ''} `}
+          >
+            <ThumbsUp style={thumbStyle} isListView={isListView} />
             {positivePercentage.toFixed(1)}%
           </div>
-          <div className="flex items-baseline">
+          <div
+            className={`flex items-baseline justify-between ${isListView ? 'w-[110px] ml-[10px]' : ''}`}
+          >
             {negativePercentage.toFixed(1)}%
-            <ThumbsDown />
+            <ThumbsDown style={thumbStyle} isListView={isListView} />
           </div>
         </div>
       </div>
     </>
   )
 }
-
-export default VoteGauge
