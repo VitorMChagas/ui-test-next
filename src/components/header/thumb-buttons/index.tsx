8c5a32d3f8f2cba66b6dcac80aca63e4ea199a44
Vitor Chagas <vitormchagas@gmail.com>
2024-03-02 19:06:32 -0300

    feat: add tablet view for thumbs at header

--- src/components/header/thumb-buttons/index.tsx
+++ src/components/header/thumb-buttons/index.tsx
@@ -1,12 +1,12 @@
 export default function ThumbButtons() {
   return (
-    <div className="flex justify-between h-[85px] w-full z-10">
+    <div className="flex justify-between h-[85px] w-full z-10 md:max-w-[368px] md:h-[38.5px]">
       <button
         className="w-1/2 h-auto bg-thumbsbg-green hover:bg-thumbsbg-green-hover"
         aria-label="thumbs up"
       >
         <img
-          className="h-8 m-auto max-w-8"
+          className="h-8 m-auto max-w-8 md:h-5"
           src="/img/thumbs-up.svg"
           alt="thumbs up"
         />
@@ -16,7 +16,7 @@ export default function ThumbButtons() {
         aria-label="thumbs down"
       >
         <img
-          className="h-8 m-auto max-w-8"
+          className="h-8 m-auto max-w-8 md:h-5"
           src="/img/thumbs-down.svg"
           alt="thumbs down"
         />
