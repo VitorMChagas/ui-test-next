1b595c202b27f91b2181ff76e5e27a85b5527d45
Vitor Chagas <vitormchagas@gmail.com>
2024-03-02 18:07:47 -0300

    feat: ajust main page with their current components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,33 @@
+'use client'
+
+import { useState } from 'react'
 import Header from '@/components/header'
 import SpeakOutButton from '@components/speak-out-button'
+import CelebrityList from '@components/celebrity-list'
+import ViewDropdown from '@components/view-dropdown'
 import CelebrityCard from '@components/celebrity-card'
 
-import { Metadata } from 'next'
-
-export const metadata: Metadata = {
-  title: 'Rule of Thumb',
-  description:
-    'Rule of Thumb™ tracks the sentiment of their users on trending and controversial people from different fields, including politics, business, media and entertainment, etc. Here users have an easy way to share their opinion on each presented celebrity using vote',
-}
-
 export default function Page() {
+  const [selectedOption, setSelectedOption] = useState('Grid')
+  function handleSelectedOption(option: string) {
+    setSelectedOption(option)
+  }
+
   return (
     <>
       <Header />
       <SpeakOutButton />
-      <div className="min-w-[1100px] h-[350px] pt-10 flex justify-center">
-        <CelebrityCard />
+      <div className="flex max-w-[1100px] justify-between mx-auto my-3 items-center px-1">
+        <p className="text-[--color-dark-gray] font-light text-[45px]">
+          Previous Rulings
+        </p>
+        <ViewDropdown
+          options={['List', 'Grid']}
+          handleSelectedOption={handleSelectedOption}
+        />
+      </div>
+      <div className="min-w-[1100px] h-[350px] flex justify-center">
+        {selectedOption === 'Grid' ? <CelebrityCard /> : <CelebrityList />}
       </div>
     </>
   )
