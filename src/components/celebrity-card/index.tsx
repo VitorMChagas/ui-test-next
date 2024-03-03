cb8a62da3d70c169e5626413b4e80c27ca4ae337
Vitor Chagas <vitormchagas@gmail.com>
2024-03-03 03:48:16 -0300

    chore: add swiper for mobile and tablet and adjust banner to mobile

--- src/components/celebrity-card/index.tsx
+++ src/components/celebrity-card/index.tsx
@@ -1,4 +1,3 @@
-'use client'
 import { Fragment } from 'react'
 import { formatDistanceStrict } from 'date-fns/formatDistanceStrict'
 import Image from 'next/image'
@@ -6,6 +5,10 @@ import ThumbsUp from '@components/thumbs-up'
 import ThumbsDown from '@components/thumbs-down'
 import VoteGauge from '@components/vote-gauge'
 import { useCelebrities } from '@hooks/useCelebrities'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import 'swiper/css'
+import useDeviceSize from '@hooks/useDeviceSize'
 
 interface HandleVoteType {
   id: number
@@ -14,6 +17,7 @@ interface HandleVoteType {
 
 export default function CelebrityCard() {
   const { celebrities } = useCelebrities()
+  const { isMobile, isTablet, isDesktop } = useDeviceSize()
 
   const handleVote = async (_id, type) => {
     try {
@@ -40,57 +44,61 @@ export default function CelebrityCard() {
   }
 
   return (
-    <div className="grid grid-cols-3 justify-items-center w-[1100px]">
-      {celebrities?.map((celebrity) => (
-        <div className="w-[348px] h-[348px] text-white mb-10 flex flex-col justify-end">
-          <Fragment key={celebrity._id}>
-            <Image
-              src={`/img/${celebrity.picture}`}
-              alt={celebrity.name}
-              width={348}
-              height={348}
-              objectFit="cover"
-              className="absolute -z-10"
-            />
-            <div className="flex max-h-[100px] flex-wrap items-center">
-              {celebrity.votes.positive > celebrity.votes.negative ? (
-                <ThumbsUp style="bg-thumbsbg-green" />
-              ) : (
-                <ThumbsDown style="bg-thumbsbg-yellow" />
-              )}
-              <p className="pl-2 overflow-hidden text-3xl font-normal whitespace-nowrap overflow-ellipsis max-w-[350px]">
-                {celebrity.name}
-              </p>
-            </div>
+    <div className="flex flex-wrap w-[375px] pl-2 md:w-[640px] lg:w-[1100px]">
+      <Swiper slidesPerView={1} centeredSlides spaceBetween={-12}>
+        {celebrities?.map((celebrity) => (
+          <SwiperSlide key={celebrity._id}>
+            <div className="w-[348px] h-[348px] text-white mb-10 flex flex-col justify-end">
+              <Fragment>
+                <Image
+                  src={`/img/${celebrity.picture}`}
+                  alt={celebrity.name}
+                  width={348}
+                  height={348}
+                  objectFit="cover"
+                  className="absolute -z-10"
+                />
+                <div className="flex max-h-[100px] flex-wrap items-center">
+                  {celebrity.votes.positive > celebrity.votes.negative ? (
+                    <ThumbsUp style="bg-thumbsbg-green" />
+                  ) : (
+                    <ThumbsDown style="bg-thumbsbg-yellow" />
+                  )}
+                  <p className="pl-2 overflow-hidden text-3xl font-normal whitespace-nowrap overflow-ellipsis max-w-[350px]">
+                    {celebrity.name}
+                  </p>
+                </div>
 
-            <p className="text-[15px] line-clamp-2 font-normal w-[300px] self-center">
-              {celebrity.description}
-            </p>
-            <p className="self-end pb-4 pr-5 text-xs font-bold">
-              {formatDistanceStrict(
-                new Date(celebrity.lastUpdated),
-                new Date(),
-              )}
-              &nbsp;ago in {celebrity.category}
-            </p>
-            <div className="flex justify-between w-[190px] self-end mr-8 mb-3">
-              <button onClick={() => handleVote(celebrity._id, 'positive')}>
-                <ThumbsUp style="bg-thumbsbg-green-hover" />
-              </button>
-              <button onClick={() => handleVote(celebrity._id, 'negative')}>
-                <ThumbsDown style="bg-thumbsbg-yellow-hover" />
-              </button>
-              <button className="w-[107px] h-[38px] border border-s-white bg-vote-now-bg text-[15px]">
-                Vote Now
-              </button>
+                <p className="text-[15px] line-clamp-2 font-normal w-[300px] self-center">
+                  {celebrity.description}
+                </p>
+                <p className="self-end pb-4 pr-5 text-xs font-bold">
+                  {formatDistanceStrict(
+                    new Date(celebrity.lastUpdated),
+                    new Date(),
+                  )}
+                  &nbsp;ago in {celebrity.category}
+                </p>
+                <div className="flex justify-between w-[190px] self-end mr-8 mb-3">
+                  <button onClick={() => handleVote(celebrity._id, 'positive')}>
+                    <ThumbsUp style="bg-thumbsbg-green-hover" />
+                  </button>
+                  <button onClick={() => handleVote(celebrity._id, 'negative')}>
+                    <ThumbsDown style="bg-thumbsbg-yellow-hover" />
+                  </button>
+                  <button className="w-[107px] h-[38px] border border-s-white bg-vote-now-bg text-[15px]">
+                    Vote Now
+                  </button>
+                </div>
+                <VoteGauge
+                  positiveVotes={celebrity?.votes?.positive}
+                  negativeVotes={celebrity?.votes?.negative}
+                />
+              </Fragment>
             </div>
-            <VoteGauge
-              positiveVotes={celebrity?.votes?.positive}
-              negativeVotes={celebrity?.votes?.negative}
-            />
-          </Fragment>
-        </div>
-      ))}
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   )
 }
