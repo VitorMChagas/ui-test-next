15fb577c15a39db027ff2bab0243c1eeb38e4a61
Vitor Chagas <vitormchagas@gmail.com>
2024-03-03 11:51:19 -0300

    feat: add all three view types in CelebrityList component

--- src/components/celebrity-list/index.tsx
+++ src/components/celebrity-list/index.tsx
@@ -39,38 +39,38 @@ export default function CelebrityList() {
   }
 
   return (
-    <div className="flex flex-col justify-items-center w-[1100px]">
+    <div className="flex flex-col justify-items-center lg:w-[1100px] w-[723px]">
       {celebrities?.map((celebrity) => (
         <div key={celebrity._id}>
-          <div className="w-[1100px] min-h-[170px] relative justify-start text-white mb-10 flex flex-col z-5 bg-[url('/img/list-bg.png')] bg-right bg-no-repeat">
-            <div className="absolute w-[267px] h-[170px]">
+          <div className="lg:w-[1100px] w-[723px] lg:min-h-[170px] min-h-[142px] relative justify-start text-white mb-10 flex flex-col z-5 lg:bg-[url('/img/list-bg.png')] bg-[url('/img/list-bg-tablet.png')] bg-right bg-no-repeat">
+            <div className="absolute lg:w-[267px] lg:h-[170px] w-[170px] h-[142px]">
               <Image
                 src={`/img/${celebrity.picture}`}
                 alt={celebrity.name}
                 fill
-                objectFit="cover"
-                className="absolute left-0 -z-10"
+                style={{ objectFit: 'cover' }}
+                className="absolute left-10 -z-10"
               />
             </div>
 
             <div className="flex max-h-[100px] flex-wrap items-center absolute top-0">
               {celebrity.votes.positive > celebrity.votes.negative ? (
                 <ThumbsUp
-                  style="bg-thumbsbg-green w-[45px] h-[45px]"
+                  thumbStyle="bg-thumbsbg-green lg:w-[45px] lg:h-[45px] w-[30px] h-[30px]"
                   isListView
                 />
               ) : (
                 <ThumbsDown
-                  style="bg-thumbsbg-yellow w-[45px] h-[45px]"
+                  thumbStyle="bg-thumbsbg-yellow lg:w-[45px] lg:h-[45px] w-[30px] h-[30px]"
                   isListView
                 />
               )}
             </div>
-            <div className="flex flex-col ml-[267px] w-max items-start">
-              <p className="overflow-hidden text-[36px] mt-[5px] font-normal whitespace-nowrap overflow-ellipsis max-w-[350px]">
+            <div className="flex flex-col lg:ml-[267px] ml-[160px] w-max items-start">
+              <p className="overflow-hidden text-[36px] lg:mt-[5px] mt-0 font-normal whitespace-nowrap overflow-ellipsis max-w-[350px]">
                 {celebrity.name}
               </p>
-              <p className="text-[15px] line-clamp-2 font-light w-[540px] self-center">
+              <p className="text-[15px] line-clamp-2 font-light lg:w-[540px] w-[340px] self-center">
                 {celebrity.description}
               </p>
               <p className="absolute right-0 px-4 py-2 text-xs font-bold bottom-self-end">
@@ -84,10 +84,10 @@ export default function CelebrityList() {
 
             <div className="flex justify-between w-[190px] self-end mr-8 mb-3 absolute -right-4 top-12">
               <button onClick={() => handleVote(celebrity._id, 'positive')}>
-                <ThumbsUp style="bg-thumbsbg-green-hover" isListView />
+                <ThumbsUp thumbStyle="bg-thumbsbg-green-hover" isListView />
               </button>
               <button onClick={() => handleVote(celebrity._id, 'negative')}>
-                <ThumbsDown style="bg-thumbsbg-yellow-hover" isListView />
+                <ThumbsDown thumbStyle="bg-thumbsbg-yellow-hover" isListView />
               </button>
               <button className="w-[107px] h-[38px] border border-s-white bg-vote-now-bg text-[15px]">
                 Vote Now
