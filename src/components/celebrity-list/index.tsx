7cc5d4c8d66163dff75506f6e3f71a6370cef764
Vitor Chagas <vitormchagas@gmail.com>
2024-03-03 19:56:14 -0300

    add: add core functionalities in CelebritiesList component

--- src/components/celebrity-list/index.tsx
+++ src/components/celebrity-list/index.tsx
@@ -1,6 +1,7 @@
 'use client'
-import { formatDistanceStrict } from 'date-fns/formatDistanceStrict'
+import { useState } from 'react'
 import Image from 'next/image'
+import { formatDistanceStrict } from 'date-fns/formatDistanceStrict'
 import ThumbsUp from '@components/thumbs-up'
 import ThumbsDown from '@components/thumbs-down'
 import VoteGauge from '@components/vote-gauge'
@@ -8,40 +9,63 @@ import { useCelebrities } from '@hooks/useCelebrities'
 
 interface HandleVoteType {
   id: number
-  type: string
+  type: 'positive' | 'negative'
 }
 
 export default function CelebrityList() {
-  const { celebrities } = useCelebrities()
+  const [votedCelebrity, setVotedCelebrity] = useState<number>()
+  const [hasVoted, setHasVoted] = useState<boolean>()
+  const [voteType, setVoteType] = useState('')
+  const { celebrities, setCelebrities } = useCelebrities()
 
-  const handleVote = async (_id, type) => {
-    try {
-      // Convert the _id to string
-      const celebrityId = String(_id)
+  const celebrityHasBeenVoted = (id: number) => votedCelebrity === id
 
-      const response = await fetch('/api/votes/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ celebrityId, type }), // Use celebrityId instead of _id
-      })
+  const handleVote = async ({ id, type }: HandleVoteType) => {
+    if (!hasVoted) {
+      try {
+        const response = await fetch('/api/votes/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ celebrityId: id, vote: type }),
+        })
 
-      if (response.ok) {
-        const data = await response.json()
-        console.log('Vote registration success:', data)
-      } else {
-        console.error('Vote registration error:', response.statusText)
+        if (response.ok) {
+          setHasVoted(true)
+          setVotedCelebrity(id)
+          const data = await response.json()
+          setCelebrities((prevCelebrity) => {
+            return prevCelebrity.map((celebrity) => {
+              if (celebrity.id === id) {
+                return {
+                  ...celebrity,
+                  votes: {
+                    ...celebrity.votes,
+                    [type]: celebrity.votes[type] + 1,
+                  },
+                }
+              }
+              return celebrity
+            })
+          })
+          console.log('Vote registration success:', data)
+        } else {
+          console.error('Vote registration error:', response.statusText)
+        }
+      } catch (error) {
+        console.error('Vote registration error:', error)
       }
-    } catch (error) {
-      console.error('Vote registration error:', error)
+    } else {
+      setVoteType('')
+      setHasVoted(false)
     }
   }
 
   return (
     <div className="flex flex-col justify-items-center lg:w-[1100px] w-[723px]">
       {celebrities?.map((celebrity) => (
-        <div key={celebrity._id}>
+        <div key={celebrity.id}>
           <div className="lg:w-[1100px] w-[723px] lg:min-h-[170px] min-h-[142px] relative justify-start text-white mb-10 flex flex-col z-5 lg:bg-[url('/img/list-bg.png')] bg-[url('/img/list-bg-tablet.png')] bg-right bg-no-repeat">
             <div className="absolute lg:w-[267px] lg:h-[170px] w-[170px] h-[142px]">
               <Image
@@ -74,24 +98,58 @@ export default function CelebrityList() {
                 {celebrity.description}
               </p>
               <p className="absolute right-0 px-4 py-2 text-xs font-bold bottom-self-end">
-                {formatDistanceStrict(
-                  new Date(celebrity.lastUpdated),
-                  new Date(),
-                )}
-                &nbsp;ago in {celebrity.category}
+                {hasVoted && celebrityHasBeenVoted(celebrity.id)
+                  ? 'Thank you for your vote!'
+                  : `${formatDistanceStrict(
+                      new Date(celebrity.lastUpdated),
+                      new Date(),
+                    )} ago in ${celebrity.category}`}
               </p>
             </div>
+            <div
+              className={`flex justify-between ${hasVoted && celebrityHasBeenVoted(celebrity.id) ? '' : 'w-[190px]'} self-end mr-8 mb-3 absolute -right-4 top-12`}
+            >
+              <>
+                {hasVoted && celebrityHasBeenVoted(celebrity.id) ? (
+                  ''
+                ) : (
+                  <>
+                    <button
+                      onClick={() => setVoteType('positive')}
+                      className="focus:border-white focus:border-[3px]"
+                    >
+                      <ThumbsUp
+                        thumbStyle="bg-thumbsbg-green-hover"
+                        isListView
+                      />
+                    </button>
+                    <button
+                      onClick={() => setVoteType('negative')}
+                      className="focus:border-white focus:border-[3px]"
+                    >
+                      <ThumbsDown
+                        thumbStyle="bg-thumbsbg-yellow-hover"
+                        isListView
+                      />
+                    </button>
+                  </>
+                )}
 
-            <div className="flex justify-between w-[190px] self-end mr-8 mb-3 absolute -right-4 top-12">
-              <button onClick={() => handleVote(celebrity._id, 'positive')}>
-                <ThumbsUp thumbStyle="bg-thumbsbg-green-hover" isListView />
-              </button>
-              <button onClick={() => handleVote(celebrity._id, 'negative')}>
-                <ThumbsDown thumbStyle="bg-thumbsbg-yellow-hover" isListView />
-              </button>
-              <button className="w-[107px] h-[38px] border border-s-white bg-vote-now-bg text-[15px]">
-                Vote Now
-              </button>
+                <button
+                  disabled={!voteType && votedCelebrity !== celebrity.id}
+                  onClick={async () => {
+                    handleVote({
+                      id: celebrity.id,
+                      type: voteType as 'positive' | 'negative',
+                    })
+                  }}
+                  className="w-[107px] h-[38px] border border-s-white bg-vote-now-bg text-[15px]"
+                >
+                  {hasVoted && celebrityHasBeenVoted(celebrity.id)
+                    ? 'Vote Again'
+                    : 'Vote Now'}
+                </button>
+              </>
             </div>
             <VoteGauge
               isListView
