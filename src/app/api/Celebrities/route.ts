853ea5fd07d1b3ac6924bfd398ff56f172203513
Vitor Chagas <vitormchagas@gmail.com>
2024-03-03 19:53:31 -0300

    feat: change celebrities get route to prisma

--- src/app/api/Celebrities/route.ts
+++ src/app/api/Celebrities/route.ts
@@ -1,12 +1,22 @@
-import Celebrity from '@service/models/Celebrity'
 import { NextResponse } from 'next/server'
-import connectToDatabase from '@service/utils/dbConnect'
+import { prisma } from '@lib/prisma'
 
 export async function GET() {
-  await connectToDatabase()
   try {
-    const celebrities = await Celebrity.find()
-    return NextResponse.json({ celebrities }, { status: 200 })
+    const celebrities = await prisma.celebrity.findMany({
+      include: {
+        votes: true,
+      },
+    })
+
+    const result = celebrities.map((item) => ({
+      ...item,
+      votes: {
+        positive: item.votes[0].positive,
+        negative: item.votes[0].negative,
+      },
+    }))
+    return NextResponse.json({ celebrities: result }, { status: 200 })
   } catch (error) {
     return NextResponse.json({ message: 'Error', error }, { status: 500 })
   }
