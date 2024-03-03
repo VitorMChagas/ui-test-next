8cb9f5649157568af4d8981f34db7e17db559026
Vitor Chagas <vitormchagas@gmail.com>
2024-03-03 19:54:36 -0300

    fix: minor z-index change in navbar

--- src/components/header/navbar/index.tsx
+++ src/components/header/navbar/index.tsx
@@ -4,7 +4,7 @@ export default function NavBar() {
   const { isDesktop } = useDeviceSize()
 
   return (
-    <div className="fixed z-10 min-h-[6rem] bg-gradient-to-b from-dark-nav pt-0 top-0 flex items-center justify-between w-full px-3 text-white md:pt-0 md:px-5 lg:mx-auto lg:px-[14%] lg:w-screen lg:min-w-[1100px]">
+    <div className="fixed z-20 min-h-[6rem] bg-gradient-to-b from-dark-nav pt-0 top-0 flex items-center justify-between w-full px-3 text-white md:pt-0 md:px-5 lg:mx-auto lg:px-[14%] lg:w-screen lg:min-w-[1100px]">
       <p className="text-[24px] font-normal lg:text-[2rem]">Rule of Thumb.</p>
       {!isDesktop ? (
         <button className="bg-[url('/img/hamburger.svg')] bg-no-repeat bg-contain h-[25px] w-[35px]" />
